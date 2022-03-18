22df2d3cba4aac5f9e82ff0e73cdf1fe9c5897eb
Ayman Zahran <ah.zahran@outlook.com>
2022-03-18 20:34:13 +0000

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,15 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Ayman Zahran',
+  authorAddress: 'ah.zahran@outlook.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'projen-cdk-tutorial-part-1',
+  repositoryUrl: 'https://github.com/AymanZahran/projen-cdk-tutorial-part-1.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
