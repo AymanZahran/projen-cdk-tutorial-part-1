37d43edcf2edc8bebb8445da1d524316486f8d6b
Ayman Zahran <ah.zahran@outlook.com>
2022-03-18 20:43:07 +0000

    Commit

--- .projenrc.js
+++ .projenrc.js
@@ -7,9 +7,33 @@ const project = new awscdk.AwsCdkConstructLibrary({
   name: 'projen-cdk-tutorial-part-1',
   repositoryUrl: 'https://github.com/AymanZahran/projen-cdk-tutorial-part-1.git',
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  description: "This package is for Projen Demo",
+
+  // // Publish to Git Release
+  release: true,
+  releaseEveryCommit: true,
+
+  // Publish to Npm
+  releaseToNpm: true,
+  packageName: 'aymanzahranpackage',
+
+  // // Publish to Maven
+  // publishToMaven: {
+  //   mavenGroupId: '<your_package_group_id',
+  //   mavenArtifactId: '<your_package_target_id>',
+  //   javaPackage: '<your_java_package>',
+  // },
+
+  // Publish to Pypi
+  publishToPypi: {
+    distName: 'aymanzahrandist',
+    module: 'aymanzahranmod',
+  },
+
+  // Publish to Nugget
+  publishToNuget: {
+    dotNetNamespace: 'aymanorg.aymanns',
+    packageId: 'aymanpackage.aymanpackagename',
+  },
 });
 project.synth();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,34 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
+
+export interface MyEcsProps {
+  readonly NumberOfAZs: 3,
+  readonly DockerFileAsset: "./dockerfiles/MyImage"
+}
+
+export class MyEcsConstruct extends Construct {
+  constructor(scope: Construct, id: string, props?: MyEcsProps) {
+    super(scope, id);
+
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      maxAzs: props.NumberOfAZs // Default is all AZs in region
+    });
+
+    const cluster = new ecs.Cluster(this, "MyCluster", {
+      vpc: vpc
+    });
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster, // Required
+      cpu: 512, // Default is 256
+      desiredCount: 6, // Default is 1
+      taskImageOptions: { 
+        image: ecs.ContainerImage.fromAsset(props.DockerFileAsset) 
+      },
+      memoryLimitMiB: 2048, // Default is 512
+      publicLoadBalancer: true // Default is false
+    });
   }
 }
