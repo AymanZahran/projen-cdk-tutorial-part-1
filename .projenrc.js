ea20e9628e90cf04bcf194bbf0fb151883894107
Ayman Zahran <ah.zahran@outlook.com>
2022-03-19 10:22:52 +0000

    Commit

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,4 @@
-const { awscdk } = require('projen');
+const { awscdk, SourceCode } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Ayman Zahran',
   authorAddress: 'ah.zahran@outlook.com',
@@ -24,23 +24,23 @@ const project = new awscdk.AwsCdkConstructLibrary({
   autoApproveUpgrades: true,
   autoApproveOptions: { allowedUsernames: ['AymanZahran'] },
 
-  // // Publish to Git Release
+  // Publish to Git Release
   release: true,
 
   // Publish to Npm
   releaseToNpm: true,
-  packageName: 'aymanzahranpackage',
+  packageName: 'ecs-package',
 
   // Publish to Pypi
   publishToPypi: {
-    distName: 'aymanzahrandist',
-    module: 'aymanzahranmod',
+    distName: 'ecs-dist',
+    module: 'ecs-mod',
   },
 
   // Publish to Nugget
   publishToNuget: {
-    dotNetNamespace: 'aymanorg.aymanns',
-    packageId: 'aymanpackage.aymanpackagename',
+    dotNetNamespace: 'ecs-org.esc-ns',
+    packageId: 'ecs-package.ecs-packagename',
   },
 
   // // Publish to Maven
@@ -59,5 +59,51 @@ const project = new awscdk.AwsCdkConstructLibrary({
 project.gitpod.addDockerImage({
   dockerFile: ".gitpod.Dockerfile"
 })
+
+function ts(path) {
+  const src = new SourceCode(project, path);
+  src.line('// ${FileBase.PROJEN_MARKER}');
+  return src
+}
+
+const EcsConstructInterface = 'MyEcsProps';
+const EcsConstructConstructor = 'MyEcsConstruct';
+const EcsConstruct = ts('src/index.ts');
+
+EcsConstruct.line(`import * as ec2 from 'aws-cdk-lib/aws-ec2';`);
+EcsConstruct.line(`import * as ecs from 'aws-cdk-lib/aws-ecs';`);
+EcsConstruct.line(`import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';`);
+EcsConstruct.line(`import { Construct } from 'constructs';`);
+
+EcsConstruct.open(`export interface ${ EcsConstructInterface } {`);
+EcsConstruct.line('readonly numberOfAzs: number;');
+EcsConstruct.line('readonly dockerfileAsset: string;');
+EcsConstruct.close('}');
+
+EcsConstruct.open(`export class ${ EcsConstructConstructor } extends Construct {`);
+EcsConstruct.open(`constructor(scope: Construct, id: string, props: ${ EcsConstructInterface }) {`);
+EcsConstruct.line(`super(scope, id);`);
+
+EcsConstruct.open(`const vpc = new ec2.Vpc(this, 'MyVpc', {`);
+EcsConstruct.line(`maxAzs: props.numberOfAzs,`);
+EcsConstruct.close('});');
+
+EcsConstruct.open(`const cluster = new ecs.Cluster(this, 'MyCluster', {`);
+EcsConstruct.line(`vpc: vpc,`);
+EcsConstruct.close('});');
+
+EcsConstruct.open(`new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'MyFargateService', {`);
+EcsConstruct.line(`cluster: cluster,`);
+EcsConstruct.line(`cpu: 512,`);
+EcsConstruct.line(`desiredCount: 6,`);
+EcsConstruct.open(`taskImageOptions: {`);
+EcsConstruct.line(`image: ecs.ContainerImage.fromAsset(props.dockerfileAsset),`);
+EcsConstruct.close(`},`);    
+EcsConstruct.line(`memoryLimitMiB: 2048,`);
+EcsConstruct.line(`publicLoadBalancer: true,`);
+EcsConstruct.close('});');
+
+EcsConstruct.close('}');
+EcsConstruct.close('}');
 
 project.synth();
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,30 @@
+// ${FileBase.PROJEN_MARKER}
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 import { Construct } from 'constructs';
-
 export interface MyEcsProps {
-  readonly numberOfAzs: 3;
-  readonly dockerfileAsset: './dockerfiles/MyImage';
+  readonly numberOfAzs: number;
+  readonly dockerfileAsset: string;
 }
-
 export class MyEcsConstruct extends Construct {
   constructor(scope: Construct, id: string, props: MyEcsProps) {
     super(scope, id);
-
     const vpc = new ec2.Vpc(this, 'MyVpc', {
-      maxAzs: props.numberOfAzs, // Default is all AZs in region
+      maxAzs: props.numberOfAzs,
     });
-
     const cluster = new ecs.Cluster(this, 'MyCluster', {
       vpc: vpc,
     });
-
     new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'MyFargateService', {
-      cluster: cluster, // Required
-      cpu: 512, // Default is 256
-      desiredCount: 6, // Default is 1
+      cluster: cluster,
+      cpu: 512,
+      desiredCount: 6,
       taskImageOptions: {
         image: ecs.ContainerImage.fromAsset(props.dockerfileAsset),
       },
-      memoryLimitMiB: 2048, // Default is 512
-      publicLoadBalancer: true, // Default is false
+      memoryLimitMiB: 2048,
+      publicLoadBalancer: true,
     });
   }
 }
